41d9bc4706b3bb963ff27ad7d83f8d5aa2edcc5c
Nick <nickp2287@gmail.com>
2020-02-17 13:03:54 +1000

    added an iterable job item for job listings page

--- src/Pages/JobList.js
+++ src/Pages/JobList.js
@@ -1,11 +1,27 @@
 import React from "react";
 import JobItem from "../PageComponents/JobItem";
+import FilterDraw from "../PageComponents/FilterDraw";
+import greenCrossLogo from "../Assets/greencross.jfif";
 
 export default function JobList() {
   return (
     <div>
       <h1>JobList works</h1>
-      <JobItem />
+      <JobItem
+        position={"Veterinarian"}
+        employType={"Permanent"}
+        businessName={"Greencross-AEC"}
+        businessLogo={greenCrossLogo}
+        location={"Brisbane-Woolloongabba"}
+        industryType={"Small Animal"}
+        pay={"$70-80k"}
+        perks={["accomodation", "car", "flight", "newgrad", "overseas"]}
+        shortDescription={
+          "seeking locum for christmas rush period, need responses soon, good $$$, accomodation and car provided, ideal candidate, blah blah blah blah blah blah blah  and something about stuff and more stuff about stuff etc."
+        }
+        datePosted={"2 Days Ago"}
+      />
+      <FilterDraw />
     </div>
   );
 }
